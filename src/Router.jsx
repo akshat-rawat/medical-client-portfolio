18b487c150acc04c3c13e904fbefdc4bd78d4ee6
Akshat Rawat <akshat010501@gmail.com>
2022-07-21 00:46:01 +0530

    build PhotoBooth for Gallery

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,10 +1,11 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AboutMe from "./pages/AboutMe";
 import Blogs from "./pages/Blogs";
 import Contact from "./pages/Contact";
 import Gallery from "./pages/Gallery";
 import Home from "./pages/Home";
 import Testimonials from "./pages/Testimonials";
+import Footer from "./components/Footer";
 
 const Router = () => {
     return <>
@@ -17,6 +18,7 @@ const Router = () => {
                 <Route path="/gallery" element={<Gallery />} />
                 <Route path="/contact" element={<Contact />} />
             </Routes>
+            <Footer />
         </BrowserRouter>
     </>
 }
