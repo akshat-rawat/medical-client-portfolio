b52d752e996bb38c5ff21eef1da4138695524637
Akshat Rawat <akshat010501@gmail.com>
2022-07-25 16:30:02 +0530

    add Blogs route

--- server/index.ts
+++ server/index.ts
@@ -1,22 +1,14 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application } from "express";
+const blogsRouter = require("./src/routes/blogs.route");
 
 const app: Application = express();
-const port = 5000;
+const port = 8080;
 
-// Body parsing Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", async (req: Request, res: Response): Promise<Response> => {
-  return res.status(200).send({
-    message: "Hello World!",
-  });
-});
+app.use("/blogs", blogsRouter);
 
-try {
-  app.listen(port, (): void => {
-    console.log(`Connected successfully on port ${port}`);
-  });
-} catch (error: any) {
-  console.error(`Error occured: ${error.message}`);
-}
+app.listen(port, () => {
+  console.log(`Connected successfully on port ${port}`);
+});
---
+++ server/src/routes/blogs.route.ts
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+
+/* GET blogs */
+router.get("/", "");
+
+/* POST blogs */
+router.post("/", "");
+
+/* PUT blogs */
+router.put("/:id", "");
+
+/* DELETE blogs */
+router.delete("/:id", "");
+
+module.exports = router;
