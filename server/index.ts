67b6304a9a2b4e2f123dbffe77475c55b34ee1e4
Akshat Rawat <akshat010501@gmail.com>
2022-07-29 17:36:11 +0530

    integrate Mongoose

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,20 @@
 import express, { Application } from "express";
+const mongoose = require("mongoose");
 const router = require("./src/routes/router");
 
 const app: Application = express();
-const port = 8080;
+const port: number = 8080;
+
+mongoose.connect("mongodb://localhost:27017/portfoliodb", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection Error:"));
+db.once("open", () => {
+  console.log("Database Connected");
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
