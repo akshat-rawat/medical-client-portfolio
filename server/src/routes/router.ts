86b13d269bc0949a7faf4fe8d7a7c384c226a42d
Akshat Rawat <akshat010501@gmail.com>
2022-08-13 16:53:41 +0530

    add Counselling & Nutrition Lifestyle in Router

--- server/src/routes/router.ts
+++ server/src/routes/router.ts
@@ -1,8 +1,12 @@
 import express from "express";
 const blogsRouter = require("./blogs/blogs.route");
+const counsellingRouter = require("./blogs/counselling.route");
+const nutrition_lifestyleRouter = require("./blogs/nutrition-lifestyle.route");
 
 const router = express.Router();
 
 router.use("/blogs", blogsRouter);
+router.use("/counselling", counsellingRouter);
+router.use("/nutrition-lifestyle", nutrition_lifestyleRouter);
 
 module.exports = router;
