e11b42fd50dcecd8a0099a3fd3ea9e7bb1ce3066
Parth Dwivedi <parthdwivedi511@gmail.com>
2022-08-20 00:13:33 +0530

    refactor Contact page

--- portfolio/src/components/Map/Map.jsx
+++ portfolio/src/components/Map/Map.jsx
@@ -1,58 +1,85 @@
 import { useState } from "react";
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Typography from "@mui/material/Typography";
 import MapGL, {
-    Marker,
-    NavigationControl,
-    FullscreenControl
+  Marker,
+  NavigationControl,
+  FullscreenControl,
 } from "react-map-gl";
 import CityPin from "./CityPin";
 import * as CONSTANTS from "../../services/constants/mapConstants";
 
 const fullscreenControlStyle = {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    padding: "10px"
+  position: "absolute",
+  top: 0,
+  left: 0,
+  padding: "10px",
 };
 
 const navStyle = {
-    position: "absolute",
-    top: 36,
-    left: 0,
-    padding: "10px"
+  position: "absolute",
+  top: 36,
+  left: 0,
+  padding: "10px",
 };
 
+const theme = createTheme({
+  typography: {
+    fontFamily: ["EB Garamond", "serif"].join(","),
+  },
+});
+
 const Map = () => {
-    const [viewport, setViewport] = useState({
-        latitude: CONSTANTS.latitude,
-        longitude: CONSTANTS.longitude,
-        zoom: 12,
-        bearing: 0,
-        pitch: 0
-    });
+  const [viewport, setViewport] = useState({
+    latitude: CONSTANTS.latitude,
+    longitude: CONSTANTS.longitude,
+    zoom: 12,
+    bearing: 0,
+    pitch: 0,
+  });
 
-    const updateViewport = newViewport => setViewport(newViewport);
+  const updateViewport = (newViewport) => setViewport(newViewport);
 
-    return (
-        <MapGL
-            {...viewport}
-            width="50vw"
-            height="50vh"
-            mapStyle="mapbox://styles/mapbox/dark-v9"
-            onViewportChange={updateViewport}
-            mapboxApiAccessToken={CONSTANTS.apiTOKEN}
-        >
-            <Marker latitude={CONSTANTS.latitude} longitude={CONSTANTS.longitude}>
-                <CityPin size={20} onClick={() => { }} />
-            </Marker>
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        {/* <div className='Map'> */}
+        {/* <div className='Map2'> */}
 
-            <div className="fullscreen" style={fullscreenControlStyle}>
-                <FullscreenControl />
-            </div>
-            <div className="nav" style={navStyle}>
-                <NavigationControl />
-            </div>
-        </MapGL>
-    );
-}
+                <div style={{ display: "flex", justifyContent: "center", marginTop: "34px", maxWidth: "90vw", paddingRight: "20px", paddingLeft: "20px" }}>
+                  <MapGL
+                    {...viewport}
+                    width="90vw"
+                    height="50vh"
+                    mapStyle="mapbox://styles/mapbox/dark-v9"
+                    onViewportChange={updateViewport}
+                    mapboxApiAccessToken={CONSTANTS.apiTOKEN}
+                  >
+                    <Marker
+                      latitude={CONSTANTS.latitude}
+                      longitude={CONSTANTS.longitude}
+                    >
+                      <CityPin size={20} onClick={() => {}} />
+                    </Marker>
+
+                    <div className="fullscreen" style={fullscreenControlStyle}>
+                      <FullscreenControl />
+                    </div>
+                    <div className="nav" style={navStyle}>
+                      <NavigationControl />
+                    </div>
+                  </MapGL>
+                </div>
+              
+
+        {/* </div> */}
+        {/* </div> */}
+      </ThemeProvider>
+    </>
+  );
+};
 
 export default Map;
--- portfolio/src/pages/Contact.jsx
+++ portfolio/src/pages/Contact.jsx
@@ -1,9 +1,67 @@
 import Map from "../components/Map";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Typography from "@mui/material/Typography";
+import { Grid } from "@mui/material";
+import "./Contact.css";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: ["EB Garamond", "serif"].join(","),
+  },
+});
 
 const Contact = () => {
-  return <>
-    <Map />
-  </>;
-}
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <div className="contact">
+          <Grid container p={2}>
+            <Grid item lg={4}>
+              <card>
+                <CardContent>
+                  <div style={{ paddingLeft: "50px" }}>
+                    <p
+                      className="para"
+                      style={{ fontSize: "2rem", textAlign: "left" }}
+                    >
+                      Address:{" "}
+                    </p>
+                    406, Bohra Colony, <br />
+                    Bohra Layout, Gottigere, <br />
+                    Bengaluru, Karnataka 560083
+                    <br />
+                    <br />
+                    <p
+                      className="para"
+                      style={{ fontSize: "2rem", textAlign: "left" }}
+                    >
+                      E-Mail:{" "}
+                    </p>
+                    drsmilki@gmail.com <br />
+                    <br />
+                    <p
+                      className="para"
+                      style={{ fontSize: "2rem", textAlign: "left" }}
+                    >
+                      Phone:{" "}
+                    </p>
+                    +91-1234567890
+                  </div>
+                </CardContent>
+              </card>
+            </Grid>
+            <Grid item lg={8}>
+              <card>
+                <Map style={{ widht: "20px" }} />
+              </card>
+            </Grid>
+          </Grid>
+        </div>
+      </ThemeProvider>
+    </>
+  );
+};
 
 export default Contact;
