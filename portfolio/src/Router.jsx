7de9c99028fa6e5a2b612322632400ec035d496b
Akshat Rawat <akshat010501@gmail.com>
2022-08-20 12:58:59 +0530

    switch to HashRouter & add Services Route

--- portfolio/src/Router.jsx
+++ portfolio/src/Router.jsx
@@ -1,9 +1,10 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 import AboutMe from "./pages/AboutMe";
+import ServicesProvided from "./pages/ServicesProvided";
 import Blogs from "./pages/Blogs";
 import Contact from "./pages/Contact";
 import Gallery from "./pages/Gallery";
-import Home from "./pages/Home";
 import Testimonials from "./pages/Testimonials";
 import Navbar from "./components/Layouts/Navbar";
 import Footer from "./components/Layouts/Footer";
@@ -11,18 +12,20 @@ import Footer from "./components/Layouts/Footer";
 const Router = () => {
   return (
     <>
-      <BrowserRouter>
+      <HashRouter>
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/about" element={<AboutMe />} />
+          <Route path="/services" element={<ServicesProvided />} />
           <Route path="/blogs" element={<Blogs />} />
           <Route path="/testimonials" element={<Testimonials />} />
           <Route path="/gallery" element={<Gallery />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
         <Footer />
-      </BrowserRouter>
+      </HashRouter>
     </>
   );
 };
---
+++ portfolio/src/pages/ServicesProvided.jsx
@@ -0,0 +1,8 @@
+
+const ServicesProvided = () => {
+    return <>
+        Services Provided
+    </>;
+}
+
+export default ServicesProvided;
